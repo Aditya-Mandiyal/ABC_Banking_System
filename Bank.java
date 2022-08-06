cba58303b6afdf960a8b26178da1429bc5053c2f
Aditya Mandiyal <mandiyaladitya@gmail.com>
2022-08-06 19:20:37 +0530

    now I use array for storing the objects (accounts) because arraylist show some errors

--- Bank.java
+++ Bank.java
@@ -15,15 +15,80 @@ class ABCbank{
 public class Bank {
 	    public static void main(String[] args){
 	    
-	    	
-	    	Scanner sc = new Scanner(System.in);  
-	        System.out.print("Type Number of Customers Who want to create Account -");  
-	        int num = sc.nextInt();  
-	        ArrayList<ABCbank> accountlist =new ArrayList<>(num); 
-	        for (int i = 0; i < accountlist.size(); i++) {  
-	            accountlist.set(i,new ABCbank());  
-	            accountlist.get(i).openAccount();  
-	        }  
+	    	 Scanner sc = new Scanner(System.in);  
+	         
+	         
+	         System.out.print("How many number of customers do you want to input? ");  
+	         int n = sc.nextInt();  
+	         ABCbank arrayOfObjects[] = new ABCbank[n];  
+	         for (int i = 0; i < arrayOfObjects.length; i++) {  
+	             arrayOfObjects[i] = new ABCbank();  
+	             arrayOfObjects[i].openAccount();  
+	         } 
+	         
+	         
+	         
+	         int key=0;  
+	         while(key!=5){  
+	             System.out.println("\n *** WELCOME IN ABC BANK ***");  
+	             System.out.println("1. Display all account details \n 2. Search by Account number\n 3. Deposit the amount \n 4. Withdraw the amount \n 5.Exit ");  
+	             System.out.println("Enter your choice: ");  
+	             key = sc.nextInt();  
+	                 switch (key) {  
+	                     case 1:  
+	                         for (int i = 0; i < arrayOfObjects.length; i++) {  
+	                             arrayOfObjects[i].showAccount();  
+	                         }  
+	                         break;  
+	                     case 2:  
+	                         System.out.print("Enter account no. you want to search: ");  
+	                         String ac_no = sc.next();  
+	                         boolean found = false;  
+	                         for (int i = 0; i < arrayOfObjects.length; i++) {  
+	                             found = arrayOfObjects[i].search(ac_no);  
+	                             if (found) {  
+	                                 break;  
+	                             }  
+	                         }  
+	                         if (!found) {  
+	                             System.out.println("Account doesn't exist..!!");  
+	                         }  
+	                         break;  
+	                     case 3:  
+	                         System.out.print("Enter Account no. : ");  
+	                         ac_no = sc.next();  
+	                         found = false;  
+	                         for (int i = 0; i < arrayOfObjects.length; i++) {  
+	                             found = arrayOfObjects[i].search(ac_no);  
+	                             if (found) {  
+	                                 arrayOfObjects[i].deposit();  
+	                                 break;  
+	                             }  
+	                         }  
+	                         if (!found) {  
+	                             System.out.println("Account doesn't exist..!!");  
+	                         }  
+	                         break;  
+	                     case 4:  
+	                         System.out.print("Enter Account No : ");  
+	                         ac_no = sc.next();  
+	                         found = false;  
+	                         for (int i = 0; i < arrayOfObjects.length; i++) {  
+	                             found = arrayOfObjects[i].search(ac_no);  
+	                             if (found) {  
+	                                 arrayOfObjects[i].withdrawal();  
+	                                 break;  
+	                             }  
+	                         }  
+	                         if (!found) {  
+	                             System.out.println("Account doesn't exist..!!");  
+	                         }  
+	                         break;  
+	                     case 5:  
+	                         System.out.println("Bye Bye !!!! See you soon...");  
+	                         break;  
+	                 }  
+	             } 
 	    }
 	    }
 	
