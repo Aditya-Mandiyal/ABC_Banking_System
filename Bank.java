46c37be389702011ef91bbf7e34653ea8117a706
Aditya Mandiyal <mandiyaladitya@gmail.com>
2022-08-06 18:20:18 +0530

    version 1.1 (create classes and Arraylist for accounts)

--- Bank.java
+++ Bank.java
@@ -1,8 +1,29 @@
 package com.godigit.banking;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+class ABCbank{
+	    private String accno;  
+	    private String name;  
+	    private String acc_type;  
+	    private long balance;
+	    
+	    public void openAccount() {
+	    	
+	    }
+}
 public class Bank {
 	    public static void main(String[] args){
 	    
+	    	
+	    	Scanner sc = new Scanner(System.in);  
+	        System.out.print("Type Number of Customers Who want to create Account -");  
+	        int num = sc.nextInt();  
+	        ArrayList<ABCbank> accountlist =new ArrayList<>(num); 
+	        for (int i = 0; i < accountlist.size(); i++) {  
+	            accountlist.set(i,new ABCbank());  
+	            accountlist.get(i).openAccount();  
+	        }  
 	    }
 	    }
 	
-
